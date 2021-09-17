ecd65b428eabca5a84a7414eda06d6e0eb0ffdda
the-intern <ostclasses@gmail.com>
2021-09-17 12:05:37 -0500

    layout and related components

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,7 +2,31 @@ import React from 'react'
 import Layout from '../components/Layout'
 const NewsLetter = () => {
   return (
-    <h4>newsletter</h4>
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get the latest posts ...</h2>
+          <h4>... and updates to current posts.</h4>
+          <form className="contact-form">
+            <input
+              type="text"
+              name="name"
+              placeholder="enter your name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="and your email"
+              className="form-control"
+            />
+            <button type="submit" className="btn form-control submit-btn">
+              Submit
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
