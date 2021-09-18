ad78d90e749b8460b6e66fd5dab91d106e7ef255
the-intern <ostclasses@gmail.com>
2021-09-17 22:22:56 -0500

    posts page working with graphql etc

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,43 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+
+//
+const IndexPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+
   return (
     <Layout>
       <Hero showPerson />
+      <Posts posts={posts} title="recent posts" />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        frontmatter {
+          author
+          category
+          date(formatString: "MMMM D, YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          readTime
+          slug
+          title
+        }
+        id
+        excerpt
+      }
+    }
+  }
+`
 
 export default IndexPage
