6d17378cc5c348a957c24059f0cc1830671fd821
the-intern <ostclasses@gmail.com>
2021-09-21 17:13:40 -0500

    trying to get number 7 post to work

--- src/components/Complete/Code.js
+++ src/components/Complete/Code.js
@@ -1,12 +1,14 @@
-import React from "react"
+import React from 'react'
 const Code = ({ children }) => {
   return (
     <code
       style={{
-        background: "var(--clr-grey-10)",
-        color: "var(--clr-grey-5)",
-        padding: "1rem",
-        borderRadius: "var(--radius)",
+        background: 'var(--clr-grey-10)',
+        color: 'var(--clr-grey-5)',
+        // padding: "1rem",
+        // trk
+        padding: '0.1rem',
+        borderRadius: 'var(--radius)',
       }}
     >
       {children}
